2799be50cfdd96ee92dda583b70cb3451050632f
Rishiraj <52316252+Rishiraj122@users.noreply.github.com>
2021-01-14 13:15:29 +0530

    Checking size

---
+++ public/exercise.js
@@ -0,0 +1,28 @@
+var images = ["https://assets4.lottiefiles.com/packages/lf20_OFt8Hh.json",
+        "https://assets6.lottiefiles.com/packages/lf20_jryyrscd.json",
+        "https://assets2.lottiefiles.com/packages/lf20_Ajcy3F.json",
+        "https://assets6.lottiefiles.com/packages/lf20_jryyrscd.json",
+        "https://assets2.lottiefiles.com/packages/lf20_cswADz.json",
+        "https://assets6.lottiefiles.com/packages/lf20_jryyrscd.json",
+        "https://assets2.lottiefiles.com/packages/lf20_igizh2.json",
+        "https://assets6.lottiefiles.com/packages/lf20_jryyrscd.json",
+        "https://assets10.lottiefiles.com/packages/lf20_DKfVfU.json",
+    ];
+        var x = 0;
+        var timer=0,value=30000;
+  
+        
+        function changeImage() {
+          document.getElementById('ad').removeAttribute("src");
+          document.getElementById('imgsrc').innerHTML = "<lottie-player src=" + images[x] +" loop  autoplay></lottie-player>";
+          if (x < 4) {
+            x += 1;
+          } else {
+            x = 0;
+          }
+        }
+        window.onload = function() {
+          setInterval(function () {
+            changeImage();
+          }, value);
+        }
