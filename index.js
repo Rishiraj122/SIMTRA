7ef42ba65b10a2571eaec6853ab4c6532e6f6509
Rishiraj <52316252+Rishiraj122@users.noreply.github.com>
2021-01-14 12:53:06 +0530

    Added workout main page conatining transitions

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ app.get("/", function(req, res) {
 
 app.get("/home", function(req,res){
     res.render("workout.ejs");
-})
+});
+
+app.get("/workout",function(req,res){
+    res.render("exercise.ejs");
+});
 
 app.get("*", function(req, res) {
     res.render("home.ejs");
-})
+});
 
 app.listen(process.env.PORT, process.env.IP, function(req, res) {
     console.log("Server has been started baby");
